1c69c64fa54b11dbdab89aac937d01f16d61589d
JuanLasso-Lint <Juane.lassob@uqvirtual.edu.co>
2025-11-12 23:11:22 -0500

    Add files via upload

---
+++ P.Final PG2/Proyecto_Final_PG_2/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.uniquindio.edu.co.poo.proyecto_final_pg_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.uniquindio.edu.co.poo.proyecto_final_pg_2 to javafx.fxml;
+    exports org.uniquindio.edu.co.poo.proyecto_final_pg_2;
+}
